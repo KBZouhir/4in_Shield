b037d313475501e2dad0db836e9000de88e64593
Zertal Raouf <61386756+Zertalraouf@users.noreply.github.com>
2021-06-22 16:17:35 +0100

    Update

---
+++ app/src/main/java/com/example/a4in_shield/child_dashboard_3.java
@@ -0,0 +1,16 @@
+package com.example.a4in_shield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import stic.cdam.a4in_shield.R;
+
+public class child_dashboard_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_dashboard3);
+    }
+}
